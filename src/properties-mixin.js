b4805a53ade222ebf271f5f836143bed0ce3c0dd
Chris Haynes <chris.haynes@gmail.com>
2018-02-09 22:56:36 +0000

    Add rIC polyfill

--- src/properties-mixin.js
+++ src/properties-mixin.js
@@ -1,5 +1,25 @@
 const setStack = [];
 
+window.requestIdleCallback =
+  window.requestIdleCallback ||
+  function (cb) {
+    var start = Date.now();
+    return setTimeout(function () {
+      cb({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        }
+      });
+    }, 1);
+  }
+
+window.cancelIdleCallback =
+  window.cancelIdleCallback ||
+  function (id) {
+    clearTimeout(id);
+  }
+
 const CompostPropertiesMixin = (parent) => {
   return class extends parent {
     static get properties() {
